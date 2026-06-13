3b5f01e7406c75864a576b9d0c63f6ec0bc05c02
kashish goswami <goswamikashish209@gmail.com>
2026-06-13 15:22:28 +0530

    Update server port config

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
-console.log("Key:",process.env.GOOGLE_GEMINI_KEY);
-console.log("Length:",process.env.GOOGLE_GEMINI_KEY.length);
 
 const app = require('./src/app')
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000,()=>{
-    console.log('server is running on http://localhost:5000')
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
 })
